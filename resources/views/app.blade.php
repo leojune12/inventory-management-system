17980932b889070b2a124158cea33d69d3a050b4
Leo <leojunebedeo@gmail.com>
2023-09-07 15:06:50 +0800

    Add body bg-color

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -20,7 +20,7 @@
             margin-bottom: 0;
         }
     </style>
-    <body class="font-sans antialiased">
+    <body class="font-sans antialiased bg-gray-100">
         @inertia
     </body>
 </html>
