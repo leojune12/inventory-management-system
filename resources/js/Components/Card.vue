75dae5cd6b5e55ca7868f9e20c846963d0c09c60
Leo <leojunebedeo@gmail.com>
2023-09-13 16:43:22 +0800

    Add card component

---
+++ resources/js/Components/Card.vue
@@ -0,0 +1,23 @@
+<template>
+    <div>
+        <div class="p-4 lg:p-6 bg-white rounded-lg shadow">
+            <div class="pb-4 rounded-t border-b mb-4">
+                <slot name="card-header"></slot>
+            </div>
+            <div>
+                <slot name="card-body"></slot>
+            </div>
+            <div>
+                <slot name="card-footer"></slot>
+            </div>
+        </div>
+    </div>
+</template>
+<script>
+export default {
+
+}
+</script>
+<style lang="">
+
+</style>
